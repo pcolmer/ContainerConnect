ec0f200ef18671bc1347a9bfd908c18838ce18a3
Philip Colmer <philip.colmer@linaro.org>
2018-11-23 10:02:53 +0000

    Initial commit

---
+++ build-and-serve-site.ps1
@@ -0,0 +1,37 @@
+if (-Not (Test-Path env:JEKYLL_ENV))
+{
+    Set-Item -path env:JEKYLL_ENV -value "staging"
+}
+
+Set-Item -path env:JEKYLL_CONFIG -value ("_config.yml,_config-"+$env:JEKYLL_ENV+".yml")
+
+$pwd = (Get-Location)
+
+# If GEM_HOME is declared, see if it is in the current directory. If not, we need
+# to volume-mount it and change the variable value.
+$params = 'run', '--rm', '-it',
+          '-e', 'JEKYLL_ACTION="serve"',
+          '-e', 'JEKYLL_CONFIG',
+          '-e', 'JEKYLL_ENV',
+          '-e', 'SOURCE_DIR',
+          '-e', 'DEST_DIR',
+          '-p', '4000:4000'
+if (Test-Path env:GEM_HOME) {
+    $gem_home = $env:GEM_HOME
+    $parent = (Split-Path -parent $gem_home)
+    if ((Join-Path $parent '') -ne $pwd)
+    {
+        $params += '-e', "GEM_HOME=""/gems/$(Split-Path -Leaf $gem_home)""", '-v', "$($parent -replace '\\', '/'):/gems"
+    }
+    else {
+        $params += '-e', 'GEM_HOME'
+    }
+}
+else {
+    New-Item -ItemType Directory -Force -Path ${pwd}/.gems
+}
+if (!Test-Path env:DEST_DIR) {
+    New-Item -ItemType Directory -Force -Path ${pwd}/dest_dir
+}
+$params += '-v', "${pwd}:/srv", 'linaroits/jekyllsitebuild', 'build-site.sh'
+& docker @params
---
+++ build-site.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -z "$JEKYLL_ENV" ]; then
+	export JEKYLL_ENV=staging
+fi
+export JEKYLL_CONFIG="_config.yml,_config-$JEKYLL_ENV.yml"
+
+# If GEM_HOME is set, see if we need to handle it being outside
+# of the current directory.
+if [ ! -z "$GEM_HOME" ]; then
+        gem_parent_dir="$(dirname "$GEM_HOME")"
+        if [ "$gem_parent_dir" != "$(pwd)" ]; then
+                # Build a volume mount command
+                # and change the value for GEM_HOME to point to it
+                GEM_VAR="-eGEM_HOME=/gems"
+                GEM_MOUNT="-v$GEM_HOME:/gems"
+        fi
+else
+        GEM_VAR=""
+        GEM_MOUNT=""
+fi
+
+if [ -z "$DEST_DIR" ] && [ ! -d "$(pwd)/dest_dir" ]; then
+	mkdir "$(pwd)/dest_dir"
+fi 
+
+docker run --rm -it -e JEKYLL_CONFIG -e JEKYLL_ENV -e SOURCE_DIR -e DEST_DIR "$GEM_VAR" \
+	"$GEM_MOUNT" -v "$(pwd)":/srv linaroits/jekyllsitebuild:latest build-site.sh
